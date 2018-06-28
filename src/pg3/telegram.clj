792d07cec53549f1bd211b4cab801467c6fd4bb5
Ilya Beda <ir4y.ix@gmail.com>
2018-06-28 21:47:09 +0700

    Cleanup pg3.telegram
    Rid of comment in notification
    Remove useless code

--- src/pg3/telegram.clj
+++ src/pg3/telegram.clj
@@ -6,43 +6,16 @@
    [morse.api :as t]
    [clojure.string :as str]))
 
+(def mock-path (System/getenv "TELEGRAM_MOCK_PATH"))
+
 (def token  (or (System/getenv "TELEGRAM_TOKEN")
                 (k8s/secret :pg3 :TELEGRAM_TOKEN)))
 
 (def chatid (or (System/getenv "TELEGRAM_CHATID")
                 (k8s/secret :pg3 :TELEGRAM_CHATID)))
 
-(def channel (atom {}))
-
-(h/defhandler handler
-  (h/command-fn "start"
-    (fn [{{id :id :as chat} :chat}]
-      (t/send-text token id " Welcome to PG3!")))
-
-  (h/command-fn "chatid"
-    (fn [{{id :id :as chat} :chat}]
-      (t/send-text token id (str "Chat-id: " id))))
-
-  (h/command-fn "help"
-    (fn [{{id :id :as chat} :chat}]
-      (t/send-text token id "/chatid - show current chatid ")))
-
-  (h/message-fn
-    (fn [{{id :id} :chat :as message}]
-      (t/send-text token id "I don't do a whole lot ... yet."))))
-
 (defn notify [msg]
   (t/send-text token chatid {:parse_mode "Markdown"} msg))
-
-(defn start []
-  (if (str/blank? token)
-    (println "Please provde token in TELEGRAM_TOKEN environment variable!")
-    (reset! channel (p/start token handler))) )
-
-(defn stop []
-  (when @channel
-    (p/stop @channel)
-    (reset! channel nil)))
 
 (defn prepare-text [text]
   (if (or (str/includes? text "_") (str/includes? text "*"))
@@ -60,19 +33,10 @@
 (def noEmoji (apply str (Character/toChars 10060)))
 
 (defn notify* [emoji phase text res]
-  (let [msg (str emoji (make-message phase text res))]
-    #_(spit "/Users/ilyabeda/telegram.txt" (str msg "\n") :append true)
-    (notify msg)))
+  (let [msg (str emoji " " (make-message phase text res))]
+    (if mock-path
+      (spit mock-path (str msg "\n") :append true)
+      (notify msg))))
 
 (def error (partial notify* noEmoji))
 (def success (partial notify* okEmoji))
-
-(defn restart []
-  (stop)
-  (start))
-
-(comment
-  (notify (str (apply str (Character/toChars 9989)) (apply str (Character/toChars 10060))) )
-  (start)
-  (stop)
-  (restart))
