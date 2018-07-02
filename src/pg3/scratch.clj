44f51f63a633b3972dd90d1d9a95241ee1d42981
Nikita Tretyakov <tretyakov.me@gmail.com>
2018-07-02 19:23:11 +0700

    Specify backup type

--- src/pg3/scratch.clj
+++ src/pg3/scratch.clj
@@ -33,6 +33,7 @@
           :replicas {:sync 1}}
    :backup {:period "1m"
             :pod-spec {:image "healthsamurai/backup-pg3:latest"
+                       :args ["sql"]
                        #_:envFrom #_[{:configMap {}}
                                      {:secretRef {}}]}}
    :config {:config {:shared_buffers "1GB"
@@ -41,6 +42,11 @@
 (comment
 
   (k8s/patch perseus-cluster)
+
+  (k8s/patch (assoc-in (k8s/find {:kind "PgBackup"
+                                  :ns "pg3"
+                                  :apiVersion "pg3.io/v1"
+                                  :id "pg3-perseus"}) [:spec :pod-spec :spec :containers 0 :args] ["sql"]))
 
   (clojure.pprint/pprint
    (k8s/find (model/postgres-deployment (:replica (ut/my-pginstances perseus-cluster))))
@@ -74,132 +80,132 @@
                             :metadata {:namespace "pg3"
                                        :name instance-name}})
                  {:phase "active"})
-  )
-
-(comment
-
-  (start)
-  (stop)
-
-  (->
-   (init)
-   debug)
-
-  (-> (k8s/query cluster-definition)
-      (debug))
-
-  (def test-db
-    {:kind "Pg"
-     :ns "default"
-     :apiVersion "pg3.io/v1"
-     :metadata {:name "cleo-prod"
-                :labels {:service "cleo"
-                         :system "pg3"
-                         :stage "prod"}}
-     :spec {:image "aidbox/aidboxdb"
-            :version "passive"
-            :size "300Gi"
-            :replicas {:sync 1}}
-     :config {:config {:shared_buffers "1GB"
-                       :max_connections 100}}})
-
-
-  (clojure.pprint/pprint (k8s/query {:ns "pg3"
-                                     :apiVersion "v1"
-                                     :kind "pod"}))
-
-  (-> (k8s/patch test-db)
-      (debug))
-
-  (-> (config-map test-db)
-      (k8s/patch)
-      (debug))
-
-  (def db-secret (secret test-db))
-
-  db-secret
-
-  (-> db-secret
-      (k8s/patch)
-      (debug))
-
-  (def test-inst-1
-    (->
-     test-db
-     (instance-spec "green" "master")))
-
-  test-inst-1
-
-  (->
-   test-inst-1
-   (k8s/patch)
-   (debug))
-
-  test-inst-1
-
-  (instance-data-volume-spec test-inst-1)
-
-  (instance-data-volume-spec test-inst-1)
-
-  (k8s/patch (instance-data-volume-spec test-inst-1))
-
-  (count (:items (k8s/query (instance-data-volume-spec test-inst-1))))
   
 
-  (create-volumes test-inst-1)
+  (comment
 
-  (-> (k8s/curl "/api/v1")
-      (json/parse-string)
-      (debug))
+    (start)
+    (stop)
 
-  (k8s/delete
-   (initdb-pod test-db db-secret test-inst-1))
-
-  (->
-   (initdb-pod test-db db-secret test-inst-1)
-   ;; (json/generate-string {:pretty true})
-   (k8s/create)
-   ;; (->> (spit "/tmp/result.yaml"))
-   (debug)
-   )
-
-
-  (-> (master-deployment test-db db-secret test-inst-1)
-      (k8s/patch)
-      (debug))
-
-  (-> (master-service test-db test-inst-1)
-      (k8s/patch)
-      (debug))
-
-
-  (def inst-slave
     (->
-     test-db
-     (instance-spec "blue" "replica")))
+     (init)
+     debug)
 
-  inst-slave
+    (-> (k8s/query cluster-definition)
+        (debug))
 
-  (k8s/create inst-slave)
+    (def test-db
+      {:kind "Pg"
+       :ns "default"
+       :apiVersion "pg3.io/v1"
+       :metadata {:name "cleo-prod"
+                  :labels {:service "cleo"
+                           :system "pg3"
+                           :stage "prod"}}
+       :spec {:image "aidbox/aidboxdb"
+              :version "passive"
+              :size "300Gi"
+              :replicas {:sync 1}}
+       :config {:config {:shared_buffers "1GB"
+                         :max_connections 100}}})
 
-  (create-volumes inst-slave)
+
+    (clojure.pprint/pprint (k8s/query {:ns "pg3"
+                                       :apiVersion "v1"
+                                       :kind "pod"}))
+
+    (-> (k8s/patch test-db)
+        (debug))
+
+    (-> (config-map test-db)
+        (k8s/patch)
+        (debug))
+
+    (def db-secret (secret test-db))
+
+    db-secret
+
+    (-> db-secret
+        (k8s/patch)
+        (debug))
+
+    (def test-inst-1
+      (->
+       test-db
+       (instance-spec "green" "master")))
+
+    test-inst-1
+
+    (->
+     test-inst-1
+     (k8s/patch)
+     (debug))
+
+    test-inst-1
+
+    (instance-data-volume-spec test-inst-1)
+
+    (instance-data-volume-spec test-inst-1)
+
+    (k8s/patch (instance-data-volume-spec test-inst-1))
+
+    (count (:items (k8s/query (instance-data-volume-spec test-inst-1))))
+    
+
+    (create-volumes test-inst-1)
+
+    (-> (k8s/curl "/api/v1")
+        (json/parse-string)
+        (debug))
+
+    (k8s/delete
+     (initdb-pod test-db db-secret test-inst-1))
+
+    (->
+     (initdb-pod test-db db-secret test-inst-1)
+     ;; (json/generate-string {:pretty true})
+     (k8s/create)
+     ;; (->> (spit "/tmp/result.yaml"))
+     (debug)
+     )
 
 
-  (->
-   (init-replica-pod test-db db-secret inst-slave)
-   (k8s/create)
-   (debug))
+    (-> (master-deployment test-db db-secret test-inst-1)
+        (k8s/patch)
+        (debug))
 
-  #_(k8s/delete
-   (init-replica-pod test-db db-secret inst-slave))
+    (-> (master-service test-db test-inst-1)
+        (k8s/patch)
+        (debug))
 
-  (-> (replica-deployment test-db db-secret inst-slave)
-      (k8s/patch)
-      (debug))
 
-  (-> (slave-service test-db inst-slave)
-      (k8s/patch)
-      (debug)
-      )
+    (def inst-slave
+      (->
+       test-db
+       (instance-spec "blue" "replica")))
 
-  )
+    inst-slave
+
+    (k8s/create inst-slave)
+
+    (create-volumes inst-slave)
+
+
+    (->
+     (init-replica-pod test-db db-secret inst-slave)
+     (k8s/create)
+     (debug))
+
+    #_(k8s/delete
+       (init-replica-pod test-db db-secret inst-slave))
+
+    (-> (replica-deployment test-db db-secret inst-slave)
+        (k8s/patch)
+        (debug))
+
+    (-> (slave-service test-db inst-slave)
+        (k8s/patch)
+        (debug)
+        )
+
+    ))
